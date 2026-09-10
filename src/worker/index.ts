9c9fee146776d55d4252b5c67e54ac0dcd78de29
READTalk <redtalk.app@hotmail.com>
2026-09-10 08:51:59 +0700

    Refactor getUser function and simplify code

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -3,35 +3,18 @@ import { CloudflareStorage } from "@openauthjs/openauth/storage/cloudflare"
 import { subjects } from "../../subjects.js"
 import { PasswordProvider } from "@openauthjs/openauth/provider/password"
 import { PasswordUI } from "@openauthjs/openauth/ui/password"
-
 interface Env { AUTH_KV: KVNamespace }
-
-async function getUser(email: string) {
-  return "123" // atau email
-}
-
+async function getUser(email: string) { return "123" }
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext) {
     return issuer({
-      storage: CloudflareStorage({ 
-        namespace: env.AUTH_KV // <-- wrangler lo namanya AUTH_KV, bukan CloudflareAuthKV
-      }),
+      storage: CloudflareStorage({ namespace: env.AUTH_KV }),
       subjects,
       providers: {
-        password: PasswordProvider(
-          PasswordUI({
-            sendCode: async (email, code) => {
-              console.log(email, code)
-            },
-          }),
-        ),
+        password: PasswordProvider(PasswordUI({ sendCode: async (email, code) => { console.log(email, code) } })),
       },
       success: async (ctx, value) => {
-        if (value.provider === "password") {
-          return ctx.subject("user", {
-            id: await getUser(value.email),
-          })
-        }
+        if (value.provider === "password") return ctx.subject("user", { id: await getUser(value.email) })
         throw new Error("Invalid provider")
       },
     }).fetch(request, env, ctx)
