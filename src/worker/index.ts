0f0880479389590b79304572b310c0c2820e496e
READTalk <redtalk.app@hotmail.com>
2026-09-10 09:01:27 +0700

    Refactor authentication to use CodeProvider

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,22 +1,17 @@
 import { issuer } from "@openauthjs/openauth"
 import { CloudflareStorage } from "@openauthjs/openauth/storage/cloudflare"
-import { subjects } from "../../subjects.js"
-import { PasswordProvider } from "@openauthjs/openauth/provider/password"
-import { PasswordUI } from "@openauthjs/openauth/ui/password"
-interface Env { AUTH_KV: KVNamespace }
-async function getUser(email: string) { return "123" }
-export default {
-  async fetch(request: Request, env: Env, ctx: ExecutionContext) {
-    return issuer({
-      storage: CloudflareStorage({ namespace: env.AUTH_KV }),
-      subjects,
-      providers: {
-        password: PasswordProvider(PasswordUI({ sendCode: async (email, code) => { console.log(email, code) } })),
-      },
-      success: async (ctx, value) => {
-        if (value.provider === "password") return ctx.subject("user", { id: await getUser(value.email) })
-        throw new Error("Invalid provider")
-      },
-    }).fetch(request, env, ctx)
+import { CodeProvider } from "@openauthjs/openauth/provider/code"
+import { subjects } from "../../subjects"
+
+export default issuer({
+  storage: CloudflareStorage({ namespace: (globalThis as any).AUTH_KV }),
+  subjects,
+  providers: {
+    code: CodeProvider({
+      sendCode: async (c, code) => console.log(c.email, code)
+    })
   },
-}
+  success: async (ctx, v) => {
+    return ctx.subject("user", { id: v.claims.email }) // ini ctx.subject yang lo approve
+  }
+})
